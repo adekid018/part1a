97b5a2fbcde2b1dd9523e6d52733f43946060eae
Adebayo Abdulazeez Adeyinka <adebayoaa519@gmail.com>
2022-08-30 19:52:59 +0100

    Part 1a excersise 1.3
    Created an obect and used it

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const Header = (props) => {
 const Part=(props)=>{
   return(
   <div>
-    <p>The name of the part on parts is {props.name} and it has {props.excersise}</p>
+    <p>The name of the part is {props.name} and it has {props.excersise}</p>
   </div>
   )
 }
@@ -34,35 +34,40 @@ const Total=(props)=>{
 /*Step 2 */
 const App=()=> {
   const course='Half stack application development'
-  const part1='Fundamentals of React'
-  const exercises1=10
-  const part2='using propsto pass data'
-  const exercises2=7
-  const part3='state of a component'
-  const exercises3=14
-
+  const part1={
+    name:'Fundamentals of React',
+    exercises:10
+  }
+  const part2={
+    name:'using propsto pass data',
+    exercises:7
+  }
+  const part3={
+  name:'state of a component',
+  exercises:14
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>{course}</h1>
         <p>
-          {part1} {exercises1}
+          {part1.name} {part1.exercises}
         </p>
         <p>
-          {part2} {exercises2}
+        {part1.name} {part2['exercises']}
         </p>
         <p>
-          {part3} {exercises3}
+        {part1.name} {part3.exercises}
         </p>
-        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+        <p>Number of exercises {part1['exercises'] + part2.exercises + part3.exercises}</p>
         <p>
           {/*Edit <code>src/App.js</code> and save to reload.*/}
           
         </p>
         <Header course={course} />
-        <Total total={exercises1+exercises2+exercises3}/>
-        <Content part1={part1} excersise1={exercises1} part2={part2} excersise2={exercises2} part3={part3} excersise3={exercises3}/>
+        <Total total={part1.exercises+part2['exercises']+part3['exercises']}/>
+        <Content part1={part1.name} excersise1={part1.exercises} part2={part2.name} excersise2={part2.exercises} part3={part3.name} excersise3={part3.exercises}/>
         <a
           className="App-link"
           href="https://reactjs.org"
