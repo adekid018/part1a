4e43280991ffcddecfdf59f610e04fbcb054ab35
Adebayo Abdulazeez Adeyinka <adebayoaa519@gmail.com>
2022-08-31 22:50:23 +0100

    Learnt State Component and Event handlers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 const Header = (props) => {
   return (
     <div>
@@ -7,72 +8,30 @@ const Header = (props) => {
     </div>
   )
 }
+const DisplayValue=({text})=><h3>The current value is {text}</h3>
+const Button=({onClicked,text})=>{
+return <button onClick={onClicked}>{text}</button>
+}
 
-const Part=(props)=>{
-  return(
-  <div>
-    <p>The name of the part is {props.name} and it has {props.excersise}</p>
-  </div>
-  )
-}
-const Content=(props)=>{
-  return(
-    <div>
-    <Part name={props.part1["parts"][0].name} excersise={props.part1["parts"][0].exercises}/>
-    <Part name={props.part1["parts"][1].name} excersise={props.part1["parts"][1].exercises}/>
-    <Part name={props.part1["parts"][2].name} excersise={props.part1["parts"][2].exercises}/>
-    </div>
-  )
-}
-const Total=(props)=>{
-  return(
-    [
-      <p>The total number of exercises is {props.total["parts"][0]["exercises"]+props.total["parts"][1]["exercises"]+props.total["parts"][2]["exercises"]}</p>
-    ]
-  )
-}
-/*Step 2 */
 const App=()=> {
+  const [counterValue, increaseCounterValue]=useState(0)
+  const increase=()=>increaseCounterValue(counterValue+1)
+  const decrease=()=>increaseCounterValue(counterValue-1)
+  const reset=()=>increaseCounterValue(0)
   const course={
-    name:'Half stack application development',
-    parts:[{
-    name:'Fundamentals of React',
-    exercises:10
-  },
-  {
-    name:'using propsto pass data',
-    exercises:7
-  },
-  {
-  name:'state of a component',
-  exercises:14
+    name:'Half stacks application development',
   }
-]}
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>{course.name}</h1>
-        <p>
-          {course.parts[0].name} {course.parts[0].exercises}
-        </p>
-        
-        <p>
-        {course.parts[1].name} {course.parts[1]['exercises']}
-        </p>
-        <p>
-        {course.parts[2].name} {course.parts[2].exercises}
-        </p>
-        
-        <p>Number of exercises {course.parts[0]['exercises'] + course.parts[1].exercises + course.parts[2].exercises}</p>
-        
-  <p>
           {/*Edit <code>src/App.js</code> and save to reload.*/}
-          
-        </p>
         <Header course={course} />
-        <Total total={course}/>
-        <Content part1={course}/>
+        <DisplayValue text={counterValue} />
+        <Button onClicked={increase} text={"Plus1"} />
+        <Button onClicked={decrease} text={"Minus1"} />
+        <Button onClicked={reset} text={"Reset"} />
         <a
           className="App-link"
           href="https://reactjs.org"
