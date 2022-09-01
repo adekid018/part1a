5caedfe3e30367a0a74294f2689da066ae4f503a
Adebayo Abdulazeez Adeyinka <adebayoaa519@gmail.com>
2022-09-01 11:15:41 +0100

    1.12*: anecdotes step1
    added a button that can be clicked to display a random anecdote from the field of software engineerng

--- src/App.js
+++ src/App.js
@@ -8,35 +8,28 @@ const Header = (props) => {
     </div>
   )
 }
-const Statistic=({outcome,text})=>{
 
-if(outcome===0){
-return (<p>No FeedBack Given Yet</p>)  
-}else{
-return (<table>
-  <tbody>
-  <tr>
-    <td>{text}</td>
-    <td>{outcome}</td>
-  </tr>
-  </tbody>
-</table>
-)
-}
-}
 const Button=({onClicked,text})=>{
 return <button onClick={onClicked}>{text}</button>
 }
 
 const App=()=> {
-  const [value, changedValue]=useState({
-    good:0,
-    bad:0,
-    neutral:0
-  })
-  const good=()=>changedValue({...value,good:value.good+1})
+  const [value, changedValue]=useState(0)
+  
+
+  /*const good=()=>changedValue({...value,good:value.good+1})
   const bad=()=>changedValue({...value,bad:value.bad+1})
-  const neutral=()=>changedValue({...value,neutral:value.neutral+1})
+  const neutral=()=>changedValue({...value,neutral:value.neutral+1})*/
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+  const random=()=>{changedValue(Math.floor(Math.random()*anecdotes.length))}
   const course={
     name:'Half stacks application development',
   }
@@ -49,16 +42,11 @@ const App=()=> {
           {/*Edit <code>src/App.js</code> and save to reload.*/}
         <Header course={course} />
 
-        <Button onClicked={good} text={"Good"} />
-        <Button onClicked={neutral} text={"Neutral"} />
-        <Button onClicked={bad} text={"Bad"} />
+        <Button onClicked={random} text={"Enter Value"} />
+        <p>{anecdotes[value]}</p>
+
         <h2>Statistic</h2>
-        <Statistic text={"Good"} outcome={value.good} />
-        <Statistic text={"Neutral"} outcome={value.neutral} />
-        <Statistic text={"Bad"} outcome={value.bad} />
-        <Statistic text={"All"} outcome={(value.good+value.bad+value.neutral)} />
-        <Statistic text={"Average:"} outcome={((value.good/1)+(value.bad/-1))/3} />
-        <Statistic text={"Positive Percentage:"} outcome={((value.good)/(value.good+value.bad+value.neutral))*100 + "%"} />
+        
         
         <a
           className="App-link"
