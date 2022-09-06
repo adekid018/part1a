b8d2f2dbb63c47fc7105f2683257b5d11c8e0304
Adebayo Abdulazeez Adeyinka <adebayoaa519@gmail.com>
2022-09-06 09:12:58 +0100

    2.4: Course information step9 part2a
    extended our application to allow for an arbitrary number of courses:

--- Part2/src/App.js
+++ Part2/src/App.js
@@ -5,41 +5,56 @@ import Course from './components/Course';
 //import { useState } from 'react';
 
 const App=()=> {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
-//const them=course.parts.map(val=>console.log(val.exercises))
-//const all=course.parts.reduce((acc,cur)=>acc+cur.exercises,0)
-//console.log(all)
+  const course = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Course  course={course}/>
-        
+      {/*  <Course  course={course}/>*/}
+      <Course  course={course}/>
         <a
           className="App-link"
           href="https://reactjs.org"
