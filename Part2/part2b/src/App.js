e212f5fec28abadcb2e1fb6cedb6dc71d805b571
Adebayo Abdulazeez Adeyinka <adebayoaa519@gmail.com>
2022-09-07 12:46:38 +0100

    2.7: The Phonebook Step2 part2b
    Prevent the user from being able to add names that already exist in the phonebook

--- Part2/part2b/src/App.js
+++ Part2/part2b/src/App.js
@@ -1,4 +1,5 @@
 
+
 import './App.css';
 import Phonebook from './components/Phonebook';
 import { useState } from 'react';
@@ -7,22 +8,39 @@ import { useState } from 'react';
 const App = () => {
   //Decleare a use state for the contact
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', 
+    id:1
+  }
   ])
   //Decleare a use state for updating the contact
   const [newName, setNewName] = useState('')
-  
+
   //an event handler used to add the contact to the person state 
   const noteSubmitted=(event)=>{
     //stopping it from submiting automatically
     event.preventDefault();
     const noteObj={
-      name:newName
+      name:newName,
+      id:persons.length + 1
     }
     //updating the person state
-    setPersons(persons.concat(noteObj))
-    //reseting the input value
+    //checking if contact exist
+    
+
+    console.log(persons.some((value)=>value.name.includes(newName)))
+    if(persons.some((value)=>value.name.includes(newName)))
+    {
+      alert(`${newName.toUpperCase()} is already added to the phonebook`)
+    }
+    else{
+      setPersons(persons.concat(noteObj))
+    }
     setNewName("")
+  
+    //reseting the input value
+  
+    
+  
   }
   //handling the changes made on the input
   const updatingName=(event)=>{
@@ -43,7 +61,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((value)=><Phonebook key={value.name} note={value.name}/>)}
+      {persons.map((value)=><Phonebook key={value.id} contact={value.name}/>)}
     </div>
   )
 }
