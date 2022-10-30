3940c459e4f4d320cf6fe96b4e612512300a6c80
Adebayo Abdulazeez Adeyinka <adebayoaa519@gmail.com>
2022-10-30 11:17:41 +0100

    Excersise 3.3
    Creating a get method with express using the res.json and making it find a particlar id

--- part3/part3a/app.js
+++ part3/part3a/app.js
@@ -22,9 +22,23 @@ let persons=[
       "number": "39-23-6423122"
     }
 ]
+const date=new Date()
+console.log(date);
 app.get('/persons',(req,res)=>{
-  console.log(res.json.persons);
     res.json(persons)
+})
+app.get('/info',(req,res)=>{
+    res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+})
+app.get('/persons/:id',(req,res)=>{
+  const note=req.params.id
+  const find=persons.find(value=>value.id===note)
+  if(find){
+    res.json(find)
+  }
+  else{
+    res.status(204).end()
+  }
 })
 /*app.get('/notes/:id',(req,res)=>{
     const myRequest=Number(req.params.id)
