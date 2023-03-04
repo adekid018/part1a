ee1969e6db70e8e9522e4d7383c9373e745c510a
unknown <adebayoaa519@gmail.com>
2023-03-04 21:20:52 +0100

    5.7 Blog list frontend, step7
    add a button to each blog, which controls whether all of the details about the blog are shown or not.

--- part5/part5b/src/components/blogs.js
+++ part5/part5b/src/components/blogs.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 const Blog=(props)=>{
+    {/*Created a state for the content visibility*/}
+    const [contentVisibility,setBlogContentVisibility]=useState(false)
+    {/*designed a style to show all the content of the blog */}
+    const showBlogContentStyle={display:contentVisibility?"":"none"}
+    {/*Toggle the value of the visibility*/}
+    const showBlogContent=()=>{
+        setBlogContentVisibility(!contentVisibility)
+    }
+    //make the value of the button change based on the useState value
+    const onClickValue=contentVisibility?"hide":"show"
     return(
     <div>
-            <p>{props.author}</p>
-            <p>{props.title}</p>
-            <a href={`https://${props.url}`}>{props.url}</a>
-            <p>{props.url}</p>
-            <p>{props.vote}</p>
-            <button onClick={props.delete}>Delete</button>
-        
+            <p>{props.title} <button onClick={showBlogContent}>{onClickValue}</button></p>
+            <div style={showBlogContentStyle}>
+                <a href={`https://${props.url}`}>{props.url}</a>
+                <p>Likes {props.vote}</p>
+                <p>{props.author}</p>
+                <button onClick={props.delete}>Delete</button>
+            </div>
     </div>
     )
 }
