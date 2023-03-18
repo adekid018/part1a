a459fb6cc89dcc9f40d5ae68a678da480de3fce0
unknown <adebayoaa519@gmail.com>
2023-03-18 10:35:36 +0100

    Added coment to the code

--- part5/part5c/src/tests/blog.test.js
+++ part5/part5c/src/tests/blog.test.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import {render,screen} from '@testing-library/react'
 import Blog from '../components/blogs'
 
+//created a test method
 test('render blog title',()=>{
+    //specified the  content
     const blogs={
         title:"How to make money",
         author:"Ade"
     }
+    //render the component
     render(<Blog title={blogs.title} author={blogs.author}/>)
     const title = screen.getByText('How to make money')
+    //check if the response is correct
     expect(title).toBeDefined()
     const author = screen.getByText('How to make money')
     expect(author).toBeDefined()
